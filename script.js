b15f5e93a84617b54ab581d55992cbaf9a1cfe2e
Syazwan Fitri <syazfitry69@gmail.com>
2022-02-04 15:13:35 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+class DigitalClock {
+    constructor(element) {
+        this.element = element;
+    }
+
+    // Start the clock upon opening, updating every half a second
+    start() {
+        this.updateTime();
+        
+        setInterval(() => {
+            this.updateTime();
+        }, 500); 
+    }
+
+    updateTime() {
+        const parts = this.getTimeParts();
+        const minutesFormatted = parts.minutes.toString().padStart(2, '0');
+        const timesFormatted = `${parts.hour}:${minutesFormatted}`;
+        const amPm = parts.isAm ? "AM" : "PM";
+
+        this.element.querySelector('.clock-time').textContent = timesFormatted;
+        this.element.querySelector('.clock-ampm').textContent = amPm;
+    }
+
+    getTimeParts() {
+        const now = new Date();
+
+        return {
+            hour : now.getHours() % 12 || 12, // returning 0-23 
+            minutes : now.getMinutes(), // returning 0-59
+            isAm : now.getHours() < 12  // returning boolean values
+        };
+    }
+}
+
+const clockElement = document.querySelector('.clock');
+const clockObject = new DigitalClock(clockElement);
+
+clockObject.start();
